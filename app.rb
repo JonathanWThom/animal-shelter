6e5838d343ec761a1a49a98dff567c767bf12584
Jonathan Thom and Tyler Ferrin <jonathan@>
2016-12-06 11:41:03 -0800

    Created customer form and routes

--- app.rb
+++ app.rb
@@ -11,3 +11,22 @@ DB = PG.connect({:dbname => "shelter_test"})
 get('/') do
   erb(:index)
 end
+
+get('/customer') do
+  erb(:new_customer_form)
+end
+
+get('/worker') do
+  erb(:worker)
+end
+
+post('/customer/new') do
+  name = params.fetch("name")
+  phone = params.fetch("phone").to_i
+  animal_preference = params.fetch("animal_preference")
+  breed_preference = params.fetch("breed_preference")
+  @human = Human.new({:name => name, :phone => phone, :animal_preference => animal_preference, :breed_preference => breed_preference, :id => nil})
+  @human.save()
+  @pets = Animal.all()
+  erb(:pets)
+end
