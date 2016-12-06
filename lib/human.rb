3a116a3564e0079f25a05523027e2460fb821765
Jonathan Thom and Tyler Ferrin <tyler@>
2016-12-06 10:03:52 -0800

    add save, equal, and attribute method/specs

--- lib/human.rb
+++ lib/human.rb
@@ -14,7 +14,7 @@ class Human
     humans = []
     returned_humans.each() do |human|
       name = human.fetch("name")
-      phone = human.fetch("phone")
+      phone = human.fetch("phone").to_i
       animal_preference = human.fetch("animal_preference")
       breed_preference = human.fetch("breed_preference")
       id = human.fetch("id").to_i
@@ -22,4 +22,14 @@ class Human
     end
     humans
   end
+
+  def save
+    result = DB.exec("INSERT INTO humans (name, phone, animal_preference, breed_preference) VALUES ('#{@name}', #{@phone}, '#{@animal_preference}', '#{@breed_preference}') RETURNING id;")
+    @id = result[0].fetch("id").to_i
+  end
+
+  def ==(another_human)
+    self.name().==(another_human.name()).&self.phone().==(another_human.phone()).&(self.animal_preference().==(another_human.animal_preference())).&(self.breed_preference().==(another_human.breed_preference())).&(self.id().==(another_human.id()))
+  end
+
 end
--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -1,9 +1,52 @@
 require('spec_helper')
 
+
+
 describe(Human) do
+  before() do
+    @human = Human.new(:name => "Jonathan", :phone => 5555555, :animal_preference => "Dog", :breed_preference => "Dachshund", :id => nil)
+  end
   describe(".all") do
     it("returns an empty array at first") do
       expect(Human.all()).to(eq([]))
+    end
+  end
+
+  describe("#name") do
+    it("returns the name") do
+      expect(@human.name()).to(eq("Jonathan"))
+    end
+  end
+
+  describe("#phone") do
+    it("returns the phone") do
+      expect(@human.phone()).to(eq(5555555))
+    end
+  end
+
+  describe("#animal_preference") do
+    it("returns the animal_preference") do
+      expect(@human.animal_preference()).to(eq("Dog"))
+    end
+  end
+
+  describe("#breed_preference") do
+    it("returns the breed_preference") do
+      expect(@human.breed_preference()).to(eq("Dachshund"))
+    end
+  end
+
+  describe("#save") do
+    it("saves an object to the database") do
+      @human.save()
+      expect(Human.all).to(eq([@human]))
+    end
+  end
+
+  describe("#id") do
+    it("returns the id") do
+      @human.save()
+      expect(@human.id()).to(be_an_instance_of(Fixnum))
     end
   end
 end
