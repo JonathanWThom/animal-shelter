f9b2eea3fc4a1da52cac504d350f18f22a32b747
Jonathan Thom and Tyler Ferrin <jonathan@>
2016-12-06 09:20:50 -0800

    Add save and equals methods to animal class

--- lib/animal.rb
+++ lib/animal.rb
@@ -20,10 +20,17 @@ class Animal
       date_of_admittance = animal.fetch("date_of_admittance")
       type = animal.fetch("type")
       breed = animal.fetch("breed")
-      human_id = animal.fetch("human_id")
+      human_id = animal.fetch("human_id").to_i()
       animals.push(Animal.new({:name => name, :gender => gender, :date_of_admittance => date_of_admittance , :type => type, :breed => breed, :human_id => human_id }))
     end
     animals
   end
 
+  define_method(:save) do
+    DB.exec("INSERT INTO animals (name, gender, date_of_admittance, type, breed, human_id) VALUES ('#{@name}', '#{@gender}', '#{@date_of_admittance}', '#{@type}', '#{@breed}', #{@human_id});")
+  end
+
+  define_method(:==) do |another_animal|
+    self.name().==(another_animal.name()).&(self.gender().==(another_animal.gender())).&(self.type().==(another_animal.type())).&(self.breed().==(another_animal.breed())).&(self.human_id().==(another_animal.human_id()))
+  end
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -2,7 +2,7 @@ require('spec_helper')
 
 describe(Animal) do
   before() do
-    @animal = Animal.new({:name => "Ernie", :gender => "Male", :date_of_admittance => "2013-05-07", :type => "Dog", :breed => "Dachshund", :human_id => 1})
+    @animal = Animal.new({:name => "Ernie", :gender => "Male", :date_of_admittance => "2013-05-07 00:00:00", :type => "Dog", :breed => "Dachshund", :human_id => 1})
   end
 
   describe(".all") do
@@ -25,7 +25,7 @@ describe(Animal) do
 
   describe("#date_of_admittance") do
     it('will return the date of admittance') do
-      expect(@animal.date_of_admittance()).to(eq("2013-05-07"))
+      expect(@animal.date_of_admittance()).to(eq("2013-05-07 00:00:00"))
     end
   end
 
@@ -44,6 +44,20 @@ describe(Animal) do
   describe("#human_id") do
     it('will return the human id') do
       expect(@animal.human_id()).to(eq(1))
+    end
+  end
+
+  describe('#save') do
+    it('adds an animal to the animal array') do
+      @animal.save()
+      expect(Animal.all()).to(eq([@animal]))
+    end
+  end
+
+  describe("#==") do
+    it('it is the same if it has the same attributes except date and human') do
+      new_animal = Animal.new({:name => "Ernie", :gender => "Male", :date_of_admittance => "2013-05-07", :type => "Dog", :breed => "Dachshund", :human_id => 1})
+      expect(new_animal).to(eq(@animal))
     end
   end
 end
